4ac805b409e45ce11a923100046ab9a19b8d9a24
vitachen <vitachen1109@gmail.com>
2021-11-26 07:18:15 +0800

    Initialize Project

---
+++ README.md
@@ -0,0 +1,45 @@
+# 餐廳清單 1.0
+![Index page about Restaurant List](./public/image/restaurant_list.png)
+
+使用 Node.js 與 Express 建立的餐廳清單，可用關鍵字搜尋和查閱餐廳資料
+
+## 功能列表
+- 可查看所有餐廳訊息
+- 可針對關鍵字搜尋
+- 點選單一餐廳，進一步瀏覽餐廳詳細資訊
+
+## 安裝說明
+1. 請先確認有安裝 node.js 與 npm
+2. 將專案 clone 到本地
+3. 在本地開啟之後，透過終端機進入資料夾，輸入：
+
+   ```bash
+   npm install
+   ```
+
+4. 安裝完畢後，繼續輸入：
+
+   ```bash
+   npm run start
+   ```
+
+5. 若看見此行訊息則代表順利運行，打開瀏覽器進入到以下網址
+
+   ```bash
+   Listening on http://localhost:3000
+   ```
+
+6. 若欲暫停使用
+
+   ```bash
+   ctrl + c
+   ```
+## 開發工具
+
+- Node.js 16.13.0
+- Express 4.17.1
+- Express-Handlebars 5.3.5
+- Bootstrap 4.3.1
+- Font-awesome 5.8.1
+
+
---
+++ app.js
@@ -0,0 +1,42 @@
+// require packages used in the project
+const express = require('express')
+const app = express()
+const exphbs = require('express-handlebars')
+const restaurantList = require('./restaurant.json')
+const port = 3000
+
+
+//express template engine 
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+// Set static files
+app.use(express.static('public'))
+
+// routes setting
+app.get('/', (req, res) => {
+  res.render('index', { restaurants: restaurantList.results })
+})
+
+
+app.get('/restaurants/:restaurant_id', (req, res) => {
+  const restaurant = restaurantList.results.find(restaurant => restaurant.id.toString() === req.params.restaurant_id)
+  res.render('show', restaurant)
+})
+
+
+app.get('/search', (req, res) => {
+  const keyword = req.query.keyword.trim()
+  const restaurants = restaurantList.results.filter(restaurant => {
+    return restaurant.name.toLowerCase().includes(keyword.toLowerCase()) || restaurant.category.includes(keyword)
+  })
+
+  res.render('index', { restaurants: restaurants, keyword: keyword })
+})
+
+// start and listen on the Express server
+app.listen(port, () => {
+  console.log(`Express is listening on localhost:${port}`)
+})
+
+
