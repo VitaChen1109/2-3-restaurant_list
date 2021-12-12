2c2c46d931df95531521730827821aa760aa9a1f
vitachen <vitachen1109@gmail.com>
2021-12-12 12:25:42 +0800

    modify mongoose config

--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -1,17 +1,9 @@
 const mongoose = require("mongoose")
 const Restaurant = require("../restaurant")
 const restaurantList = require("../../restaurant.json").results
+const db = require("../../config/mongoose")
 
-mongoose.connect("mongodb://localhost/restaurant-list", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
 
-const db = mongoose.connection
-// 連線異常
-db.on('error', () => {
-  console.log('mongodb error!')
-})
 
 // 連線成功
 db.once('open', () => {
